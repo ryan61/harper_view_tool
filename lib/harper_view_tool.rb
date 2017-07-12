b7b032f1ac47fd5c60d3c45bfa959d99dec8c401
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-12 17:07:09 +0000

    Dev tool initial comit

---
+++ lib/harper_view_tool.rb
@@ -0,0 +1,6 @@
+require "harper_view_tool/version"
+require "harper_view_tool/renderer"
+
+module HarperViewTool
+  # Your code goes here...
+end
---
+++ lib/harper_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HarperViewTool
+  VERSION = "0.1.0"
+end
